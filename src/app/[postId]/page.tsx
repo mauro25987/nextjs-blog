d6f7ed3561603164b30d0244ca627903c4ffa706
Mauro Gonzalez <maurogonzalezbruno@gmail.com>
2025-03-17 21:25:05 -0300

    fix params

--- src/app/[postId]/page.tsx
+++ src/app/[postId]/page.tsx
@@ -13,22 +13,35 @@ type Comment = {
 async function fetchPostData(
   postId: string
 ): Promise<{ post: Post; user: User; comments: Comment[] }> {
-  const postResponse = await fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`)
-  const post = await postResponse.json()
+  try {
+    const postResponse = await fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`)
+    if (!postResponse.ok) {
+      throw new Error('Error consulta post')
+    }
+    const post = await postResponse.json()
 
-  const userResponse = await fetch(`https://jsonplaceholder.typicode.com/users/${post.userId}`)
-  const user = await userResponse.json()
+    const userResponse = await fetch(`https://jsonplaceholder.typicode.com/users/${post.userId}`)
+    if (!userResponse.ok) {
+      throw new Error('Error consulta user')
+    }
+    const user = await userResponse.json()
 
-  const commentsResponse = await fetch(
-    `https://jsonplaceholder.typicode.com/posts/${postId}/comments`
-  )
-  const comments = await commentsResponse.json()
-
-  return { post, user, comments }
+    const commentsResponse = await fetch(
+      `https://jsonplaceholder.typicode.com/posts/${postId}/comments`
+    )
+    if (!commentsResponse.ok) {
+      throw new Error('Error consulta comentarios')
+    }
+    const comments = await commentsResponse.json()
+    return { post, user, comments }
+  } catch (error) {
+    console.log('Error al obtener datos:', error)
+    throw error
+  }
 }
 
-export default async function Page({ params }: { params: { postId: string } }) {
-  const { postId } = await params
+export default async function Page({ params }: { params: Promise<{ postId: string }> }) {
+  const postId = (await params).postId
   const { user, post, comments } = await fetchPostData(postId)
   return (
     <div>
